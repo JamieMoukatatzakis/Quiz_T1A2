04eb55c8a56f18ac1be1c7d2ceaa82f7589ba232
Jamie <jamie221095@hotmail.com>
2019-11-19 12:55:50 +1100

    Fixed some typo's

--- quiz.rb
+++ quiz.rb
@@ -13,9 +13,9 @@ p2 = "What is Arnold Schwarzenegger's most famous role? \n(a)Conan\n(b)The Termi
 p3 = "What animal is Nemo from the movie Finding Nemo? \n(a)Dog\n(b)Rabbit\n(c)Fish"
 p4 = "What song has spent the most time on Billboards Number 1 Spot? \n(a)Despacito\n(b)Old Town Road\n(c)One Sweet Day"
 p5 = "Which artist has the most Number 1 singles? \n(a)Elvis Presley\n(b)Michael Jackson\n(c)The Beatles"
-p6 = "What is the most sold album world wide?\n(a)Back in Black by AC/DC\n(b)Thriller by Micahel Jackson\n(c)Bat out of Hell by Meatloaf"
+p6 = "What is the most sold album world wide?\n(a)Back in Black by AC/DC\n(b)Thriller by Michael Jackson\n(c)Bat out of Hell by Meatloaf"
 p7 = "What is the most sold gaming console of all time?\n(a)Xbox 360\n(b)Nintendo Wii\n(c)Playstation 2"
-p8 = "Who is the most recognised video game character of all time?\n(a)Mario\n(b)Solid Snack\n(c)Pac-Man"
+p8 = "Who is the most recognised video game character of all time?\n(a)Mario\n(b)Solid Snake\n(c)Pac-Man"
 p9 = "How many ghosts are there in Pac-Man?\n(a)2\n(b)3\n(c)4"
 
 movie_questions = [
@@ -37,6 +37,7 @@ gaming_questions = [
 puts "Welcome, Please choose a Category.\n(1)Movies\n(2)Music\n(3)Gaming"
 user_input = gets.chomp.to_s
 
+
 def run_quiz test
     answer = ""
     score = 0
@@ -53,10 +54,9 @@ def run_quiz test
             score += 1
         end
     end
-    puts "Congratulations! You got #{score} from 3"
+    puts "Congratulations! You got #{score} from 3 for this section"
 end
 
-puts user_input
 test = ""
 if user_input == '1'
     test = movie_questions
